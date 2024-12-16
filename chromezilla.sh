a49a87ba164cb6d959557a3b30e2c12d4086c54a
lanreenlight <159959899+lanreenlight@users.noreply.github.com>
2024-12-16 14:47:06 +0100

    Update chromezilla.sh
    echo

--- chromezilla.sh
+++ chromezilla.sh
@@ -25,9 +25,9 @@ echo -e " ${MAGENTA}~${YELLOW}|  \\| | | | | | | |  _|   / / | || |   | |     / 
 echo -e " ${MAGENTA}~${CYAN}| |\\  | |_| | |_| | |___ / /_ | || |___| |___ / ___ \\ ${MAGENTA}~"
 echo -e " ${MAGENTA}~${BLUE}|_| \\_|\\___/|____/|_____/____|___|_____|_____/_/   \\_\\ ${MAGENTA}~"
 echo -e " ${CYAN}~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~${RESET}"
-}
 echo "Join Nodzillaz on Telegram: https://t.me/nodezilla101"
 echo "Join Nodzillaz on Discord: https://discord.gg/RAEnTZSEVh"
+}
 
 # Check and install Docker and Docker Compose
 install_docker() {
